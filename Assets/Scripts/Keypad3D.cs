3b41a3dc41814a28598b7097aa5bf4334dfe882c
chrbachmann <christof.bachmann@aau.at>
2025-11-29 23:07:47 +0100

    Add: Keypad Attempts

--- Assets/Scripts/Keypad3D.cs
+++ Assets/Scripts/Keypad3D.cs
@@ -14,9 +14,13 @@ public class Keypad3D : MonoBehaviour
     public UnityEvent onWrongCode;
 
     [Header("Optional Display (TextMeshPro)")]
-    public TextMeshPro display; // or TextMeshPro (3D)
-
+    public TextMeshPro display;
+    public TextMeshPro attempts;
     private string currentInput = "";
+    
+    private bool codeEntered = false;
+    
+    private int numberOfAttempts = 0;
 
     private void Awake()
     {
@@ -25,11 +29,15 @@ public class Keypad3D : MonoBehaviour
 
     public void PressKey(string digit)
     {
+        if (codeEntered)
+        {
+            ResetKeypad();
+            codeEntered = false;
+        }
         if (currentInput.Length >= 4)
             return;
 
         currentInput += digit;
-        Debug.Log("Pressed: " + digit + " | Current: " + currentInput);
 
         if (display != null)
             display.text = currentInput;
@@ -44,20 +52,49 @@ public class Keypad3D : MonoBehaviour
         {
             Debug.Log("Correct Code!");
             onCorrectCode?.Invoke();
+            display.color = Color.green;
+            display.fontSize = 4;
+            display.text = "Granted";
         }
-        else
+        else if(numberOfAttempts == 2)
         {
             Debug.Log("Wrong Code!");
             onWrongCode?.Invoke();
+            display.color = Color.red;
+            display.fontSize = 4;
+            display.text = "Locked";
+            numberOfAttempts++;
         }
-
-        ResetKeypad();
+        else 
+        {
+            Debug.Log("Wrong Code!");
+            onWrongCode?.Invoke();
+            display.color = Color.red;
+            display.fontSize = 4;
+            display.text = "Denied";
+            if (numberOfAttempts == 0)
+            {
+                attempts.text = ".";
+            }
+            if (numberOfAttempts == 1)
+            {
+                attempts.text = "..";
+            }
+            numberOfAttempts++;
+        }
+        codeEntered = true;
+        
     }
 
     private void ResetKeypad()
     {
         currentInput = "";
         if (display != null)
-            display.text = "";
+        {
+             display.text = "";
+             display.fontSize = 5;
+             display.color = Color.white;
+        }
+           
     }
 }
