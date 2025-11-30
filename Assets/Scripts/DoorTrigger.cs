0ac8c635cf86feddf77caa9fad1252ec5eb5dbb3
Christoph Wedenig <christoph@wedenig.org>
2025-11-30 12:35:30 +0100

    andi lights in sample scene, working cell escape

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -46,6 +46,7 @@ namespace DefaultNamespace
                     3f,
                     child.transform.position.z);
             }
+            this.gameObject.SetActive(false);
         }
     }
 }
